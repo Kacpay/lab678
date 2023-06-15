01227b86b3b302a59732aeddd62c7363304a89d2
Kacper <student48955@dsw.edu.pl>
2023-06-15 17:09:31 +0200

    zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .json

--- task2.sh
+++ task2.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-data="{ \"key\": \"value\" }"
-echo "$data" > plik.json
+
+if ! data=$(cat plik.json 2>/dev/null); then
+  echo "Błąd odczytu pliku."
+  exit 1
+fi
+
+if ! jq . >/dev/null 2>&1 <<< "$data"; then
+  echo "Nieprawidłowa składnia pliku JSON."
+  exit 1
+fi
+
+echo "Plik JSON jest poprawny."
 
